091e64006c27e5a2140483feadef398cc3f2119f
argo <arghyasaha26@gmail.com>
2017-03-26 18:32:55 +0530

    AngularJS 2 basic hello world

---
+++ app.ts
@@ -0,0 +1,15 @@
+import { bootstrap }  from "angular2/platform/browser";
+import { Component }  from "angular2/core";
+
+@Component({
+  selector: 'reddit',
+  template: `
+  <div>Hello Argo</div>
+  `
+})
+
+class HelloWorld{
+
+}
+
+bootstrap(HelloWorld);
