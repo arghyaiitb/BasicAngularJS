885727b05b5e0042affe069d6b20143c48735b86
argo <arghyasaha26@gmail.com>
2017-03-28 11:52:11 +0530

    AngularJS2 for loop basics

--- app.ts
+++ app.ts
@@ -1,15 +1,24 @@
 import { bootstrap }  from "angular2/platform/browser";
 import { Component }  from "angular2/core";
+import { NgFor } from "angular2/common";
 
 @Component({
   selector: 'reddit',
   template: `
-  <div>Hello Argo</div>
+  <ul>
+      <li *ngFor="#name of names" > Hello {{ name }} </li>
+  </ul>
+  
   `
 })
 
 class HelloWorld{
+  names: string[];
 
+  constructor()
+  {
+    this.names = ['Argo', 'Arghya'];
+  }
 }
 
 bootstrap(HelloWorld);
